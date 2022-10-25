090b3cea314d0fd5484366201f6c5f6cb9912c07
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-24 22:42:33 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -90,30 +90,29 @@ def pregunta_01():
     # Retorne `X` y `y`
     return X, y
 
-
 def pregunta_02():
     """
     Preparación del dataset.
     """
 
     # Importe train_test_split
-    from ____ import ____
+    from sklearn.model_selection import train_test_split
 
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     X, y = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 123. Use 50 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    tamaño_prueba=(50/len(X))
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X,
+        y,
+        test_size=tamaño_prueba,
+        random_state=123,
     )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
     return X_train, X_test, y_train, y_test
-
 
 def pregunta_03():
     """
