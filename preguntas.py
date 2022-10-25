e48b8aaa9ed88829f88b5947bc016de74388e7e9
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-24 22:48:55 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -171,12 +171,12 @@ def pregunta_04():
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
     cfm_train = confusion_matrix(
         y_true = y_train,
-        y_pred = gridSearchCV.predict(x_train),
+        y_pred = pipeline.predict(x_train),
     )
 
     cfm_test = confusion_matrix(
         y_true=y_test,
-        y_pred=gridSearchCV.predict(x_test),
+        y_pred=pipeline.predict(x_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
